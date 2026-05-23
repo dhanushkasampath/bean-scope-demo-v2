e174d8f12a7e915aa9b293de89b2c1d78e677ade
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2026-05-23 21:14:28 +0800

    first commit

---
+++ src/main/java/com/learn/bean_scope_demo_v2/BeanScopeDemoV2Application.java
@@ -0,0 +1,13 @@
+package com.learn.bean_scope_demo_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanScopeDemoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanScopeDemoV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/bean_scope_demo_v2/BeanScopeDemoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.bean_scope_demo_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanScopeDemoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
